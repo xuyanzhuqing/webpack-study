ef8928ee2b1e78d544616c2a4bdf6b88c94a64b6
michael <962275976@qq.com>
2022-10-25 17:29:40 +0800

    加载数据

--- 01-manage-loader/datas/src/index.js
+++ 01-manage-loader/datas/src/index.js
@@ -1,7 +1,23 @@
 import { join } from 'lodash-es'
 import './index.css'
 
-console.info(blue)
+import Data from './data.xml';
+import Notes from './data.csv';
+
+console.info(Data, Notes)
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   var element = document.createElement('div');
--- 01-manage-loader/datas/webpack.config.js
+++ 01-manage-loader/datas/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports  = {
   entry: './src/index.js',
@@ -15,7 +18,36 @@ module.exports  = {
           { loader: 'style-loader', options: { injectType: 'singletonStyleTag' } },
           'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      },
     ]
   }
 }
