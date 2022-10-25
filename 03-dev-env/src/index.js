27dfbeb540a05a4ec61d9d84ce566978a01035e0
michael <962275976@qq.com>
2022-10-25 18:18:20 +0800

    prepare dev env

---
+++ 03-dev-env/src/index.js
@@ -0,0 +1,18 @@
+import { join } from 'lodash-es';
+import printMe from './print.js';
+
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+   element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 demo 项目采用软连接方式连接node_modules
 
 ln -s ../node_modules <./node_modules 可缺省>
+
+todo
+
+- https://www.npmjs.com/package/mini-css-extract-plugin
+- https://www.npmjs.com/package/file-loader
+- https://www.npmjs.com/search?ranking=popularity&q=HtmlWebpackPlugin
