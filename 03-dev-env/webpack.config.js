2caddcaf775608e03a9800300cfa62d32ec539f6
michael <962275976@qq.com>
2022-10-26 10:53:59 +0800

    初识webpack-dev-server

--- 03-dev-env/webpack.config.js
+++ 03-dev-env/webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  // devtool: 'inline-source-map',
   entry: {
     index: './src/index.js',
     print: './src/print.js'
@@ -13,10 +12,17 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
   plugins: [
     new HtmlWebpackPlugin({
        title: 'development',
        template: 'index.html'
     })
-  ]
+  ],
+  optimization: {
+    runtimeChunk: 'single'
+  }
 }
