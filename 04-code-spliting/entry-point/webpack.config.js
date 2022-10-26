8ef3dfdb6258f1ee7945c769caff13b9d613e6b6
michael <962275976@qq.com>
2022-10-26 14:00:36 +0800

    mutiple entry

--- 04-code-spliting/entry-point/webpack.config.js
+++ 04-code-spliting/entry-point/webpack.config.js
@@ -9,6 +9,8 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    // clean: true
+  },
+  optimization: {
+    runtimeChunk: 'single'
   }
 }
