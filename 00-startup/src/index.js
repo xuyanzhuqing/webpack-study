3579a2a850c782630e18a3872a75e80eb768af6e
michael <962275976@qq.com>
2022-10-31 11:25:42 +0800

    start-up template

---
+++ 00-startup/src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash-es'
+
+function component () {
+  const div = document.createElement('div')
+  div.innerText = _.join(['hello', 'world'])
+}
+
+document.body.appendChild(component())
