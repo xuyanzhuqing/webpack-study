d23b0c0bc59b67be68e1db9f6a8b37cb2594c643
michael <962275976@qq.com>
2022-10-31 18:43:24 +0800

    ts module config

--- 10-typescript/basic/src/index.ts
+++ 10-typescript/basic/src/index.ts
@@ -1,8 +1,15 @@
 import { join } from 'lodash-es'
+import './index.css'
+import Mark from './mark.jpg'
 
 function component () {
   const div = document.createElement('div')
   div.innerText = join(['hello', 'world'])
+
+  const img = document.createElement('img')
+  img.src = Mark
+
+  div.appendChild(img)
   return div
 }
 
--- 10-typescript/basic/webpack.config.js
+++ 10-typescript/basic/webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
@@ -13,5 +14,28 @@ module.exports = {
       title: '管理输出',
       template: 'index.html'
    })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: 'style-loader', options: { injectType: 'linkTag', attributes: { id: 'test' } } },
+          { loader: 'file-loader', options: { name: '[path][name].[ext]?[contenthash]' } }
+        ]
+      },
+      {
+        test: /\.(png|jif|jpeg|jpg||svg|)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
 }
